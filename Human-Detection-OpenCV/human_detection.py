a8105190619dfb04a5f9b70c2b04e13ad3876621
Mishab <113359139+MISHAB404@users.noreply.github.com>
2023-10-27 21:37:03 +0530

    Human detection using http request

--- Human-Detection-OpenCV/human_detection.py
+++ Human-Detection-OpenCV/human_detection.py
@@ -1,15 +1,18 @@
 import cv2
 import numpy as np
+import requests
 
-# Load YOLO model
+# YOLO model
 net = cv2.dnn.readNet("yolov4.cfg", "yolov4.weights")
 
-# Load COCO dataset class names
+# COCO dataset class names
 with open("coco.names.txt", "r") as f:
     classes = f.read().strip().split("\n")
 
-# Initialize webcam
-cap = cv2.VideoCapture(0)  # 0 represents the default camera
+cap = cv2.VideoCapture(0)  
+nodeMCU_ip = "192.168.1.8"
+turn_on_route = "/turnOn"
+turn_off_route = "/turnOff"
 
 while True:
     ret, frame = cap.read()
@@ -17,7 +20,7 @@ while True:
     if not ret:
         break
 
-    # Perform object detection
+      # Perform human detection
     blob = cv2.dnn.blobFromImage(frame, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
     net.setInput(blob)
     outs = net.forward(net.getUnconnectedOutLayersNames())
@@ -36,29 +39,31 @@ while True:
                     class_name = classes[class_id]
                     if class_name == "person":
                         has_human = True
-                        # Extract the bounding box coordinates
                         center_x = int(detection[0] * frame.shape[1])
                         center_y = int(detection[1] * frame.shape[0])
                         width = int(detection[2] * frame.shape[1])
                         height = int(detection[3] * frame.shape[0])
                         
-                        # Calculate the coordinates of the top-left and bottom-right corners of the bounding box
                         top_left = (center_x - width // 2, center_y - height // 2)
                         bottom_right = (center_x + width // 2, center_y + height // 2)
 
-                        # Draw a colored square around the person
                         cv2.rectangle(frame, top_left, bottom_right, (0, 255, 0), 2)
-
                         break
 
     if has_human:
         print("Human Detected")
+        # Send an HTTP request to turn the lights on
+        requests.get(f"http://{nodeMCU_ip}{turn_on_route}")
     else:
         print("No Humans")
+        # Send an HTTP request to turn the lights off
+        requests.get(f"http://{nodeMCU_ip}{turn_off_route}")
+
+        
 
     cv2.imshow("Human Detection", frame)
 
-    if cv2.waitKey(1) == 27:  # Press 'ESC' key to exit
+    if cv2.waitKey(1) == 27:
         break
 
 cap.release()
